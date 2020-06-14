b7013bded702c84832a37eaa8309907f94d2880c
Jasper Catthoor <jasper.catthoor@enovates.com>
2020-06-14 13:06:20 +0200

    Commands start with ! instead of ? now

--- src/main/java/com/catthoor/TipOfTheSpearBot/CommandHandler.java
+++ src/main/java/com/catthoor/TipOfTheSpearBot/CommandHandler.java
@@ -10,11 +10,11 @@ import java.util.Optional;
 public class CommandHandler {
 
     private final Map<CommandKey, Command> router = Map.ofEntries(
-            Map.entry(new CommandKey("?pc", false), new PlayerCountCommand()),
-            Map.entry(new CommandKey("?announcement", true), new AnnouncementCommand()),
-            Map.entry(new CommandKey("?auth", true), new AuthCommand()),
-            Map.entry(new CommandKey("?server", true), new ServerCommand()),
-            Map.entry(new CommandKey("?servers", false), new ServersCommand()));
+            Map.entry(new CommandKey("!pc", false), new PlayerCountCommand()),
+            Map.entry(new CommandKey("!announcement", true), new AnnouncementCommand()),
+            Map.entry(new CommandKey("!auth", true), new AuthCommand()),
+            Map.entry(new CommandKey("!server", true), new ServerCommand()),
+            Map.entry(new CommandKey("!servers", false), new ServersCommand()));
 
     public void performOnLaunchCommandActions(DiscordClient client) {
         router.values().forEach(command -> {
